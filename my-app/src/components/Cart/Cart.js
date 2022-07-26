f2f34f0ee742cc2327cd9086c752ff70237d29b9
Cata Olivar <cataolivar@gmail.com>
2022-07-26 12:34:24 -0300

    error

--- my-app/src/components/Cart/Cart.js
+++ my-app/src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, Component } from "react";
 import { cartContext } from "../../context/CartContext/CartContext";
 import "./Cart.css"
 import { Link } from "react-router-dom"
@@ -20,19 +20,32 @@ const Cart = () => {
         )
     }
 
-    const datosComprador = {
-        nombre: "sjdfjd",
-        telefono: 17634544,
-        email: "ahdjasd"
+    class datosComprador extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                nombre: " ",
+                telefono: " ",
+                email: " "
+               }
+        }
+      
     }
 
+        syncNameChanges (nombre); {
+        this.setState({nombre:nombre})
+    }
+
+
     const finalizarCompra = () => {
-        const ventasCollection = collection (db, "ventas")
+        const ventasCollection = collection(db, "ventas")
         addDoc(ventasCollection, {
             datosComprador,
-            items: products.map(prod => ({id: prod.id, nombre: prod.name, precio:prod.price})),
+            items: products.map(prod => ({ nombre: prod.name, precio: prod.price, id: prod.id })),
             total: calcularTotal(),
-            fecha: serverTimestamp (),})}
+            fecha: serverTimestamp(),
+        })
+    }
 
 
     return (
@@ -54,7 +67,16 @@ const Cart = () => {
                 <button className="buttonCarrito" onClick={clear}>Vaciar Carrito</button>
             </div>
             <div>
-                <button onClick={finalizarCompra} className="buttonCarrito1" >Finalizar Compra </button>
+                <form>
+                    <input type="text"
+                           value={this.state.nombre} 
+                           onChange={(ev)=>{this.syncNameChanges(ev.target.value)}} 
+                           placeholder="Nombre"/>
+
+                    <input type="text"  value={this.state.telefono} placeholder="Telefono"/>
+                    <input type="email" value={this.state.email}  placeholder="Email"/>
+                    <button onClick={finalizarCompra} className="buttonCarrito" >Finalizar Compra </button>
+                </form>
             </div>
         </div>
     );
