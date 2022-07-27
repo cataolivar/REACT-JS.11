b6a82bb7346c9b238859b6ae1227194dbf358159
Cata Olivar <cataolivar@gmail.com>
2022-07-26 22:08:09 -0300

    error

--- my-app/src/components/Cart/Cart.js
+++ my-app/src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
-import React, { useContext, Component } from "react";
+import React, { useContext } from "react";
+import { useForm } from "react-hook-form"
 import { cartContext } from "../../context/CartContext/CartContext";
 import "./Cart.css"
 import { Link } from "react-router-dom"
@@ -8,6 +9,8 @@ import { collection, addDoc, serverTimestamp } from "firebase/firestore"
 
 const Cart = () => {
     const { products, clear, calcularTotal, deleteProduct } = useContext(cartContext)
+    const { register, handleSubmit } = useForm()
+
 
     if (products.length === 0) {
         return (
@@ -20,66 +23,58 @@ const Cart = () => {
         )
     }
 
-    class datosComprador extends Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                nombre: " ",
-                telefono: " ",
-                email: " "
-               }
-        }
-      
+    const datosComprador = (data) => {
+        console.log(data)
     }
 
-        syncNameChanges (nombre); {
-        this.setState({nombre:nombre})
-    }
+const finalizarCompra = () => {
+    const ventasCollection = collection(db, "ventas");
+    addDoc(ventasCollection, {
+        datosComprador,
+        items: products.map(prod => ({ nombre: prod.name, precio: prod.price, id: prod.id })),
+        total: calcularTotal(),
+        fecha: serverTimestamp(),
+    })
+}
 
 
-    const finalizarCompra = () => {
-        const ventasCollection = collection(db, "ventas")
-        addDoc(ventasCollection, {
-            datosComprador,
-            items: products.map(prod => ({ nombre: prod.name, precio: prod.price, id: prod.id })),
-            total: calcularTotal(),
-            fecha: serverTimestamp(),
-        })
-    }
-
-
-    return (
+return (
+    <div>
+        <h1 className="carritoh1">Carrito</h1>
         <div>
-            <h1 className="carritoh1">Carrito</h1>
-            <div>
-                {products.map((prod) => (
-                    <div className="divCarrito" key={prod.id}>
-                        <img className="imgCart" src={prod.img} alt="Carrito" />
-                        <h3>Producto: {prod.name}</h3>
-                        <h3>${prod.price}</h3>
-                        <h4>Cantidad: {prod.qty}</h4>
-                        <button className="delete" onClick={() => deleteProduct(prod.id)}>Eliminar</button>
-                    </div>
-                ))}
-            </div>
-            <div className="buttonYtotal">
-                <h2 className="carritoh2">Total: ${calcularTotal()}</h2>
-                <button className="buttonCarrito" onClick={clear}>Vaciar Carrito</button>
-            </div>
-            <div>
-                <form>
-                    <input type="text"
-                           value={this.state.nombre} 
-                           onChange={(ev)=>{this.syncNameChanges(ev.target.value)}} 
-                           placeholder="Nombre"/>
-
-                    <input type="text"  value={this.state.telefono} placeholder="Telefono"/>
-                    <input type="email" value={this.state.email}  placeholder="Email"/>
-                    <button onClick={finalizarCompra} className="buttonCarrito" >Finalizar Compra </button>
-                </form>
-            </div>
+            {products.map((prod) => (
+                <div className="divCarrito" key={prod.id}>
+                    <img className="imgCart" src={prod.img} alt="Carrito" />
+                    <h3>Producto: {prod.name}</h3>
+                    <h3>${prod.price}</h3>
+                    <h4>Cantidad: {prod.qty}</h4>
+                    <button className="delete" onClick={() => deleteProduct(prod.id)}>Eliminar</button>
+                </div>
+            ))}
         </div>
-    );
+        <div className="buttonYtotal">
+            <h2 className="carritoh2">Total: ${calcularTotal()}</h2>
+            <button className="buttonCarrito" onClick={clear}>Vaciar Carrito</button>
+        </div>
+        <div>
+
+            <h2 className="h2Cart">Complete el formulario para finalizar la compra</h2>
+
+            <form className="formCart" onSubmit= {handleSubmit (datosComprador)}>
+              <label >Nombre</label>
+              <input  className="inputCart" type="text" {...register("nombre", {required: true})}/>
+
+              <label>Celular</label>
+              <input  className="inputCart" type="text" {...register("celular", {required: true})}/>
+
+              <label>Email</label>
+              <input  className="inputCart" type="text" {...register("email", {required: true})}/>
+
+                <input type="submit" value="Enviar" onClick={finalizarCompra} className="buttonSubmit"/>
+            </form>
+        </div>
+    </div>
+);
 };
 
 export default Cart;
